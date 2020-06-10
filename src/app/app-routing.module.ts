1d9f827a379e781f74f7a2b505f8f8c51a5bfe4d
Wassim Malleh <wassimmalleh.wm@gmail.com>
2020-06-10 19:55:23 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InvoiceBuilderComponent } from './invoice-builder/invoice-builder.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'invoice-builder',
+    loadChildren: './invoice-builder/invoice-builder.module#InvoiceBuilderModule'
+  },
+  {
+    path: '**',
+    redirectTo: 'invoice-builder'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/invoice-builder/invoice-builder-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { InvoiceBuilderComponent } from './invoice-builder.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: InvoiceBuilderComponent,
+    children: [
+      {
+        path: '',
+        component: MainContentComponent
+      }
+    ]
+  },
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class InvoiceBuilderRoutingModule { }
